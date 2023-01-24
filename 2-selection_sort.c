9b23602dd768bba1f99739533cf3aa49346e2af6
peterbrian254 <peterbrian8957@gmail.com>
2023-01-24 05:42:37 -0800

    new

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,47 +1,32 @@
 #include "sort.h"
 
 /**
- * swap_values - swaps 2 values in an array of ints
- *
- * @array: the array of ints
- * @i1: index of first value
- * @i2: index of 2nd value
- *
- * Return: the array with value
+ * selection_sort - A function that use selection sort algorithm.
+ * @array: An array to sort.
+ * @size: The size of the array.
+ * Return: Nothing.
  */
-
-void swap_values(int **array, ssize_t i1, ssize_t i2)
-{
-	int tmp;
-
-	tmp = (*array)[i1];
-	(*array)[i1] = (*array)[i2];
-	(*array)[i2] = tmp;
-}
-
-/**
- * selection_sort - sorts an array of integers using selection sort
- *
- * @array: the array of integers
- * @size: the size of the array
- */
-
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
+	int aux = 0;
+	size_t i = 0, j = 0, pos = 0;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL || size == 0)
+		return;
+
+	for (; i < size - 1; i++)
 	{
-		min = i;
+		pos = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
-				min = j;
+			if (array[j] < array[pos])
+				pos = j;
 		}
-
-		if (min != i)
+		if (pos != i)
 		{
-			swap_values(&array, min, i);
+			aux = array[i];
+			array[i] = array[pos];
+			array[pos] = aux;
 			print_array(array, size);
 		}
 	}
